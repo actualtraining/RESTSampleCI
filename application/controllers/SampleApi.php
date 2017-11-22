87be6c1edef893cbe31e6d08e43f9cf7bc154747
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-11-22 14:55:55 +0700

    menambahkan fungsi tambah dan edit

--- application/controllers/SampleApi.php
+++ application/controllers/SampleApi.php
@@ -13,7 +13,39 @@ class SampleApi extends REST_Controller {
         return $this->response($data,200);
     }*/
 
-    
+    /*public function index_get(){
+        //$data = array("budi","bambang","joni","joko");
+        $data = array(array("Nim"=>"78999898","Nama"=>"Erick"),
+        array("Nim"=>"89898765","Nama"=>"Budi"));
+        return $this->response($data,200);
+    }*/
+
+    public function index_get(){
+        $nim = $this->get('nim');
+        $nama = $this->get('nama');
+
+        $data = array("Nim"=>$nim,"Nama"=>$nama);
+        return $this->response($data,200);
+    }
+
+    //untuk insert data
+    public function index_post(){
+        $nim = $this->post('nim');
+        $nama = $this->post('nama');
+        $alamat = $this->post('alamat');
+
+        return $this->response("Data ".$nim." berhasil ditambah !",200);
+    }
+
+    //untuk edit data
+    public function index_put(){
+        $nim = $this->put('nim');
+        $nama = $this->put('nama');
+        $alamat = $this->put('alamat');
+
+        $result = "Berhasil mengupdate data ".$nim.", ".$nama.",  ".$alamat;
+        return $this->response($result,200);
+    }
 }
 
 
