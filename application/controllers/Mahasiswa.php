56bc4a03ea705d0aa1c559714d3cdcd3047d9756
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-11-22 16:20:40 +0700

    menambahkan add method

--- application/controllers/Mahasiswa.php
+++ application/controllers/Mahasiswa.php
@@ -7,15 +7,33 @@ class Mahasiswa extends REST_Controller{
         parent::__construct();
     }
 
-    public function index_get(){
-        $nim = $this->get('nim');
+    public function index_get($nim){
+        //$nim = $this->get('nim');
         if($nim==''){
             $data = $this->db->get('mahasiswa')->result();
         }else{
             $this->db->where('nim',$nim);
             $data = $this->db->get('mahasiswa')->result();
         }
-        
         return $this->response($data,200);
     }
+
+    public function index_post(){
+        $nim = $this->post('nim');
+        $nama = $this->post('nama');
+        $email = $this->post('email');
+        $ipk = $this->post('ipk');
+
+        $data = array("nim"=>$nim,
+        "nama"=>$nama,"email"=>$email,
+        "ipk"=>$ipk);
+
+        $result = $this->db->insert('mahasiswa',$data);
+        if($result){
+            return $this->response($data,200);
+        }else {
+            return $this->response(array("status"=>"gagal insert",502));
+        }
+    }
+
 }
