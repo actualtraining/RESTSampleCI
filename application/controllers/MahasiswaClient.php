c76410b3c5a00953a1e7df713e834bb98f4d8899
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-11-23 11:17:01 +0700

    menambahkan curl

--- application/controllers/MahasiswaClient.php
+++ application/controllers/MahasiswaClient.php
@@ -1,15 +1,21 @@
 <?php
 
 class MahasiswaClient extends CI_Controller{
+    var $API = "";
     function __construct(){
         parent::__construct();
+        $this->API = "http://localhost/restapi/index.php";
     }
 
     public function index(){
-        $data = file_get_contents("http://localhost/restapi/index.php/Mahasiswa");
-        $results = json_decode($data);
+        //cara pertama tanpa menggunakan curl
+        /*$data['mahasiswa'] =  
+        json_decode(file_get_contents("http://localhost/restapi/index.php/Mahasiswa"));*/
 
-        echo var_dump($results);
+        $data['mahasiswa'] = json_decode(
+            $this->curl->simple_get($this->API."/Mahasiswa"));
+        
+        return $this->load->view('mahasiswa_list',$data);
     }
 }
 
---
+++ application/views/mahasiswa_list.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Daftar Mahasiswa</title>
+</head>
+<body>
+    <h2>Daftar Mahasiswa</h2>
+    <table border="1">
+        <tr><th>Nim</th><th>Nama</th><th>Email</th><th>IPK</th></tr>
+        <?php 
+        foreach($mahasiswa as $mhs){
+            echo "<tr><td>".$mhs->nim."</td>
+            <td>".$mhs->nama."</td>
+            <td>".$mhs->email."</td>
+            <td>".$mhs->ipk."</td></tr>";
+        }
+        ?>
+    </table>
+</body>
+</html>
